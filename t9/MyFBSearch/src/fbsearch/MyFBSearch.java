c57afa4ccb89c1dda5076d6c5c3ae3227c459f3a
Ana Veroneze <alsolorzano@inf.ufsm.br>
2016-07-02 16:46:01 -0300

    saving images and started view

--- t9/MyFBSearch/src/fbsearch/MyFBSearch.java
+++ t9/MyFBSearch/src/fbsearch/MyFBSearch.java
@@ -11,9 +11,15 @@ import com.restfb.FacebookClient;
 import com.restfb.Parameter;
 import com.restfb.Version;
 import com.restfb.types.User;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import static java.lang.System.out;
+import java.net.URL;
 import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -30,21 +36,33 @@ public class MyFBSearch {
     public void search(){
         
         Connection<User> profilesFound = fbClient.fetchConnection("search", User.class, 
-                Parameter.with("q", "ana veroneze"), Parameter.with("type", "user"),
+                Parameter.with("q", "ana veroneze solórzano"), Parameter.with("type", "user"),
                 Parameter.with("limit", 5000), Parameter.with("offset", 0));    
         
         
         List<User> pages = profilesFound.getData();
         for(User p : pages){
            //get user profile's picture
-           User user = fbClient.fetchObject(p.getId(), User.class, Parameter.with("fields", "picture, first_name, last_name, gender, name"));
+           User user = fbClient.fetchObject(p.getId(), User.class, Parameter.with("fields", "picture"));
            out.println(p.getName() + " " + p.getId());
            out.println(user.getPicture().getUrl());
-        }
+           
+           String url = user.getPicture().getUrl();
+           //get the image by the url
+            try {
+                BufferedImage img = ImageIO.read(new URL(url));
+             /* save image
+                File outputfile = new File("saved.jpg"); #save the image
+                ImageIO.write(img, "jpg", outputfile);
+            */   
+            } catch (IOException e) {
+                Logger.getLogger(MyFBSearch.class.getName()).log(Level.SEVERE, null, e);
+            }
         
-        out.println("Number of profiles found: " + profilesFound.getData().size());   
+        out.println("Number of profiles found: " + profilesFound.getData().size());    
        
-    } 
+        }
+    }
    
     public static void main(String[] args) {
         
